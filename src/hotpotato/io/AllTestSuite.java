85fd81143bcdf00961f6c05c65e2dcd8bb19a778
Eric Herman <eric@freesa.org>
2009-06-03 09:21:53 -0500

    lgpl-2.1

--- src/hotpotato/io/AllTestSuite.java
+++ src/hotpotato/io/AllTestSuite.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2003 - 2009 by Eric Herman. 
- * For licensing information see GnuGeneralPublicLicenseVersion2.txt 
- *  or http://www.fsf.org/licenses/gpl.txt
+ * For licensing information see GnuLesserGeneralPublicLicense-2.1.txt 
+ *  or http://www.gnu.org/licenses/lgpl-2.1.txt
  *  or for alternative licensing, email Eric Herman: eric AT freesa DOT org
  */
 package hotpotato.io;
--- src/hotpotato/util/ClassUtil.java
+++ src/hotpotato/util/ClassUtil.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (C) 2005 by Eric Herman.
- * For licensing information see GnuGeneralPublicLicenseVersion2.txt
- *  or http://www.fsf.org/licenses/gpl.txt 
+ * For licensing information see GnuLesserGeneralPublicLicense-2.1.txt
+ *  or http://www.gnu.org/licenses/lgpl-2.1.txt 
  *  or for alternative licensing, email Eric Herman: eric AT freesa DOT org 
  */
 package hotpotato.util;
